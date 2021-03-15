d5fb5ea5abb2df5002a9833736db65d770aec2b5
Mike Sheldon <m.sheldon@tools4ever.com>
2021-03-15 12:30:16 -0700

    Docs updates and improvements (#4)
    * Documentation
    
    * Coding Standards/Improvements
    
    * Create config.png
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,10 +1,56 @@
 # HelloID-Conn-Prov-Source-PowerSchool-SIS-Students
+<p align="center">
+  <img src="Assets/Logo.png">
+</p>
 HelloID Provisioning Source Connector for PowerSchool SIS
 
-## API Setup
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+* [Getting Started](#getting-started)
+* [Setting up the API access](#setting-up-the-api-access)
+* [Configuration](#configuration)
+
+<!-- GETTING STARTED -->
+## Getting Started
+By using this connector you will have the ability to import powerschool data into HelloID:
+
+* Student Demographics
+* Student School Enrollments
+
+## Setting up the API Access
 - Click System Settings. The System Settings page appears.
 - Click Plugin Management Configuration. The Plugin Management Dashboard page appears.
 - Click Install. The Plugin Install page appears.
 - For the Plugin Installation File, see  [Tools4ever_plugin.zip](Assets/Tools4ever_plugin.zip)
 - Click Install. A confirmation message appears. The plugin appears in the Installed Plugins section on the Plugin Management Dashboard page.
 - Retrieve Client ID and Secret
+
+## Setup the PowerShell connector
+1. Add a new 'Source System' to HelloID and make sure to import all the necessary files.
+
+    - [ ] configuration.json
+    - [ ] person.ps1
+    - [ ] department.ps1
+
+2. Fill in the required fields on the 'Configuration' tab. See also, [Setting up the API access](#setting-up-the-api-access)
+
+![image](Assets/config.png)
+* Base URI
+  * URL of the PowerSchool Instance
+* Client Key
+* Client Secret
+* Expansions
+  * Using element expansion increases performance of your application by allowing requests for only subsets of the resource and not the whole resource itself. By limiting the requests to only the data that is needed, the size and bandwidth of the response, the load on the server, and how much time it takes for the request to make a round-trip are all reduced. Each resource with expandable elements will publish them in the result using the @expansions attribute. 
+  
+  Examples = demographics, addresses, alerts, phones, school_enrollment, ethnicity_race, contact, contact_info, initial_enrollment, schedule_setup, fees, lunch
+* Extensions
+  * Resource extensions (1:1) are resources that extend core resources. They can be defined by the system, the installed state package, or by user-created extensions. They are based on PowerSchool schema extensions. They behave like, and are requested like, . Each resource with extensions will publish them in the default result using the @extensions attribute. Zero to many elements can be selected. Unknown extension requests are ignored. If the resource is writable, such as Student, then the user-defined extensions to that resource are also writable.
+
+  Examples = studentcorefields,c_studentlocator
+* Filter
+  * Filter the students to be returned. By default, active and pre-registered are included
+* Page
+  * Default size is 100. Allowed size is 100-1000
+
+_For more information about our HelloID PowerShell connectors, please refer to our general [Documentation](https://docs.helloid.com/hc/en-us/articles/360012557600-Configure-a-custom-PowerShell-source-system) page_
--- departments.ps1
+++ departments.ps1
@@ -1,62 +1,135 @@
-#Config
+#region Configuration
 $config = ConvertFrom-Json $configuration;
 $expansions = $config.expansions -split (",");
 $extensions = $config.extensions -split (",");
 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+#endregion Configuration
 
-#Get OAuth Token
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-$Token = [System.Convert]::ToBase64String( [System.Text.Encoding]::ASCII.GetBytes("$($config.apiKey):$($config.apiSecret)") );
-$headers = @{ Authorization = "Basic " + $Token };
-$tokenResponse = Invoke-RestMethod -uri "$($config.baseurl)/oauth/access_token" -Method 'POST' -Headers $headers -Body (@{grant_type= "client_credentials";})
+#region Functions
+function New-AccessToken() {
+    [cmdletbinding()]
+    Param (
+        [object]$config
+    ) 
+    Process
+    {
+        #Get OAuth Token
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+        $Token = [System.Convert]::ToBase64String( [System.Text.Encoding]::ASCII.GetBytes("$($config.apiKey):$($config.apiSecret)") );
+        $headers = @{ Authorization = "Basic " + $Token };
+        $tokenResponse = Invoke-RestMethod -uri "$($config.baseurl)/oauth/access_token" -Method 'POST' -Headers $headers -Body (@{grant_type= "client_credentials";})
 
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Bearer $($tokenResponse.access_token)")
-$headers.Add("Accept", "application/json")
 
-#Get Schools
-Write-Verbose -Verbose "Retrieving Schools"
-$uri = "$($config.baseurl)/ws/v1/district/school/count"
-$count = (Invoke-RestMethod $uri -Method GET -Headers $headers ).resource.count
-$page = 1;
-$schools = [System.Collections.ArrayList]@();
-while($true)
-{
-    $parameters = @{
-        page = $page;
-        pagesize = 100;
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Authorization", "Bearer $($tokenResponse.access_token)")
+        $headers.Add("Accept", "application/json")
+
+        return $headers;
     }
-    $uri = "$($config.baseurl)/ws/v1/district/school"
-    $response = Invoke-RestMethod $uri -Method GET -Headers $headers -Body $parameters
+}
+
+
+function Get-ObjectProperties 
+{
+    [cmdletbinding()]
+    param (
+        [object]$Object, 
+        [int]$Depth = 0, 
+        [int]$MaxDepth = 10
+    )
+    $OutObject = @{};
+
+    foreach($prop in $Object.PSObject.properties)
+    {
+        if ($prop.TypeNameOfValue -eq "System.Management.Automation.PSCustomObject" -or $prop.TypeNameOfValue -eq "System.Object" -and $Depth -lt $MaxDepth)
+        {
+            $OutObject[$prop.Name] = Get-ObjectProperties -Object $prop.Value -Depth ($Depth + 1);
+        }
+        else
+        {
+            $OutObject[$prop.Name] = "$($prop.Value)";
+        }
+    }
+    return $OutObject;
+}
+
+function Get-ErrorMessage
+{
+    [cmdletbinding()]
+    param (
+        [object]$Response
+    )
+    $reader = New-Object System.IO.StreamReader($Response.Exception.Response.GetResponseStream())
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    Write-Error "StatusCode: $($Response.Exception.Response.StatusCode.value__)`nStatusDescription: $($Response.Exception.Response.StatusDescription)`nMessage: $($reader.ReadToEnd())"
+}
+#endregion Functions
+
+#region Execute
+try {
+    $headers = New-AccessToken -Config $config;
+
+    #Get Schools
+    Write-Information "Retrieving Departments (Schools)"
+    $uri = "$($config.baseurl)/ws/v1/district/school/count"
+    $count = (Invoke-RestMethod $uri -Method GET -Headers $headers ).resource.count
+    $page = 1;
+    $schools = [System.Collections.ArrayList]@();
     
-    if($response.schools.school -is [array])
+    while($true)
     {
-        [void]$schools.AddRange($response.schools.school);
-    }
-    else
-    {
-        [void]$schools.Add($response.schools.school);
+        $parameters = @{
+            page = $page;
+            pagesize = $config.pageSize;
+        }
+        $uri = "$($config.baseurl)/ws/v1/district/school"
+        
+        
+        try {
+            Write-Information "Retrieving $($uri) - Page $($page)"
+            $response = Invoke-RestMethod $uri -Method GET -Headers $headers -Body $parameters
+        }    
+        catch {
+            Get-ErrorMessage -response $_;
+            throw $_;
+        }
+
+        if($response.schools.school -is [array])
+        {
+            [void]$schools.AddRange($response.schools.school);
+        }
+        else
+        {
+            [void]$schools.Add($response.schools.school);
+        }
+
+        if($schools.count -lt $count)
+        {
+            $page++;
+        }
+        else
+        {
+            break;
+        }
+
     }
 
-    if($schools.count -lt $count)
+    foreach($s in $schools)
     {
-        $page++;
-    }
-    else
-    {
-        break;
-    }
-
-}
-
-foreach($s in $schools)
-{
-    $row = @{
-              ExternalId = $s.id;
-              DisplayName = $s.name;
-              Code = $s.school_number;
-    }
+        $row = @{
+                  ExternalId = $s.id;
+                  DisplayName = $s.name;
+                  Code = $s.school_number;
+        }
  
-    $row | ConvertTo-Json -Depth 10
+        $row | ConvertTo-Json -Depth 10
+    }
 }
+catch
+{
+
+}
+#endregion Execute
